30b733763f75a4609cd8655f54786951d44a4d64
callnirajgupta <callnirajgupta@gmail.com>
2024-04-30 10:05:54 +0530

    Added test Listerner

--- src/test/java/org/example/test/LoginTest.java
+++ src/test/java/org/example/test/LoginTest.java
@@ -1,10 +1,12 @@
 package org.example.test;
 
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+//@Listeners(org.example.util.TestListener.class) we can add at class level or directly to testNg.xml
 public class LoginTest extends BaseTest {
 
         @Test
